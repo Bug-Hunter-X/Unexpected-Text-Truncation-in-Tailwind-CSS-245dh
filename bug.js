fd27b87259a0bc6f732a624460ef46c0287faaad
BugHunter-X <askzenai@gmail.com>
2025-01-18 04:22:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+<div class="bg-gray-100 p-4">
+  <p class="text-xl font-bold">Uncommon Tailwind CSS Bug</p>
+  <p>This is a paragraph with some text that has a relatively long line length. This long line length might be causing issues because of Tailwind's default line-clamp behaviour, leading to unexpected text truncation or overflow.</p>
+</div>
+```
